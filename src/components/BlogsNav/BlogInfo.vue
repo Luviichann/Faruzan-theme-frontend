7c20d3d7a35c0e7a05fa8155762e2ae561f12a07
Luviichann <anddiaoling777@163.com>
2024-06-20 10:21:12 +0800

    fls2

--- src/components/BlogsNav/BlogInfo.vue
+++ src/components/BlogsNav/BlogInfo.vue
@@ -7,11 +7,17 @@
                 </div>
                 <div class="blog-info leftfix">
                     <h4>{{blog.title}}</h4>
-                    <p class="intro">{{truncatedText}}</p>
+                    <p class="intro" @mouseenter="showFullText = true" @mouseleave="showFullText = false">{{truncatedText}}</p>
+                    <transition name="fade">
+                        <div v-if="showFullText" class="full-text">{{blog.text}}</div>
+                    </transition>
                 </div>
             </div>
             <div class="small-web">
-                <img :src="blog.webpct" alt="" class="small-web">
+                <img :src="blog.webpct" alt="" class="small-web" @mouseenter="showImage()" @mouseleave="hideImage()">
+                <transition name="fade">
+                    <img class="big-img" :src="blog.webpct" alt="" v-if="showBigImage">
+                </transition>
             </div>
         </div>
     </li>
@@ -24,7 +30,9 @@
         props:['blog'],
         data() {
             return {
-                showFullText:false
+                showFullText:false,
+                showBigImage:false,
+                timer:null
             }
         },
         computed: {
@@ -36,8 +44,14 @@
             }
         },
         methods: {
-            MouseEnter(){
-                // this.show
+            showImage(){
+                this.timer = setTimeout(()=> {
+                    this.showBigImage = true
+                },600)
+            },
+            hideImage(){
+                clearTimeout(this.timer)
+                this.showBigImage = false
             }
         },
     }
@@ -71,17 +85,48 @@
     .blog-info p{
         width: 204px;
         height: 34px;
+        text-overflow: ellipsis;
     }
     li{
+        /* cursor: pointer; */
         float: left;
         margin: 8px;
         padding: 8px;
         /* background-color: rgb(135, 206, 235,0.3); */
         background-color: rgba(159, 241, 228, 0.5);
         border-radius: 10px;
+        transition: background-color 0.5s ease;
+    }
+    li:hover {
+        background-color: rgba(87,130,225,0.4);
     }
     .blog-info h4{
         font-weight: bold;
         margin-bottom: 3px;
+    }
+    .full-text {
+        width: 194px;
+        position: absolute;
+        background-color: rgb(0,0,0,0.6);
+        color: #ffffff;
+        padding: 5px;
+        /* transition: opacity 0.5s ease-in-out; */
+        z-index: 10;
+        white-space: normal;
+        border-radius: 10px;
+    }
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity 0.4s;
+    }
+    .fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
+        opacity: 0;
+    }
+    .big-img{
+        height: 324px;
+        width: 576px;
+        position: absolute;
+        margin-left: -158px;
+        margin-top: 10px;
+        border-radius: 10px;
     }
 </style>
