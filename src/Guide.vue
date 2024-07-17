44e6b2407a0e3495f7af762989867cdb2527f2cd
Luviichann <anddiaoling777@163.com>
2024-07-17 10:09:03 +0800

    引导页

--- src/Guide.vue
+++ src/Guide.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        akshdkjashkjahk
+    <div class="container pointer">
+        <router-link to="/index"><h1>点击进入</h1></router-link>
     </div>
 </template>
 
@@ -11,6 +11,19 @@
     }
 </script>
 
-<style>
-    
+<style scoped>
+    .container{
+        background-color: #00000077;
+        text-align: center;
+        margin: 0 auto;
+        margin-top: 300px;
+        padding: 10px;
+        width: 400px;
+        border-radius: 20px;
+    }
+    h1{
+        margin: 0 auto;
+        font-size: 70px;
+        color: rgb(131,254,249);
+    }
 </style>
