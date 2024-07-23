03c96a8609954f4cd3485fe8fd3a8ed001216d40
Luviichann <anddiaoling777@163.com>
2024-07-23 19:55:33 +0800

    devtools

--- public/js/devtools.js
+++ public/js/devtools.js
@@ -2,14 +2,8 @@
 document.addEventListener('keydown', function(event) {
     if (event.key === 'F12' || (event.ctrlKey && event.shiftKey && event.key === 'I')) {
         event.preventDefault();
-        window.close();  // 关闭当前页面
     }
   });
-  
-  // 监听窗口大小变化
-  window.addEventListener('resize', function(e) {
-    if (window.outerWidth - window.innerWidth > 100 || window.outerHeight - window.innerHeight > 100) {
-      e.preventDefault();
-      window.close();  // 关闭当前页面
-    }
-  });
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
